127e1e2343c89c58983fb67176b7b4703282d06d
biloldii <bilol@Abdukahhors-MacBook-Pro.local>
2025-05-07 23:00:14 +0500

    Add models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='kitobkhonho'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+
+db=SQLAlchemy(app)
+login=LoginManager(app)
+login.login_view='login'
 
 from app import routes
